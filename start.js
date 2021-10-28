01bd5c2ac5aaef643c3ea123fd020921fa9fb4ad
Syun Lee <pysyun@vitche.com>
2021-10-28 19:40:03 +0300

    1. Added a resource limit to respawn the process.

--- start.js
+++ start.js
@@ -1,7 +1,27 @@
 #!/usr/bin/env node
+const {spawn} = require('child_process');
+const ResourceLimit = require("nodejs-resource-limit");
+
 require("dotenv").config();
+
+const restartProcess = () => {
+  spawn(process.argv[1], process.argv.slice(2), {
+    detached: true,
+    stdio: ['ignore']
+  }).unref()
+  process.exit()
+}
+
 const buildEvaluator = require("./main").buildEvaluator;
 const namespace = process.env.ONION_REDIS_NAMESPACE;
 buildEvaluator(namespace, () => {
   console.log(`Connected '${namespace}.PythonEvaluator' class to OnionRedis`);
 });
+
+new ResourceLimit()
+  .periodical(60000)
+  .countDown()
+  .to(process.env.RESPAWN_INTERVAL_MINUTES)
+  .execute(() => {
+    restartProcess();
+  });
